09a90f2accba80b663bdf7b94d75a833eda5b8d0
Anton Liada <liadaantom@gmail.com>
2022-12-26 18:00:37 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const { userController } = require('./controllers/users');
 
 const app = express();
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
